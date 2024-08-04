29095afe3ad3736c1c3e2ddd7f82faf75b0081f7
peyman-borhani <peyman.borhani@gmail.com>
2024-08-04 20:18:43 +0330

    +Add: example  components from bits and shad

---
+++ src/lib/components/ui/breadcrumb/breadcrumb-list.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { cn } from "$lib/utils.js";
+	export let el = undefined;
+	let className = undefined;
+	export { className as class };
+</script>
+
+<ol
+	bind:this={el}
+	class={cn(
+		"flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5",
+		className
+	)}
+	{...$$restProps}
+>
+	<slot />
+</ol>
---
+++ src/lib/components/ui/breadcrumb/breadcrumb-page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { cn } from "$lib/utils.js";
+	export let el = undefined;
+	export let className = undefined;
+	export { className as class };
+</script>
+
+<span
+	bind:this={el}
+	role="link"
+	aria-disabled="true"
+	aria-current="page"
+	class={cn("font-normal text-foreground", className)}
+	{...$$restProps}
+>
+	<slot />
+</span>
