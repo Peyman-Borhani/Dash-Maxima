00a3ad78999f42538227f5cf0fe8f54ad94fd6b3
peyman-borhani <peyman.borhani@gmail.com>
2024-08-05 14:33:36 +0330

    -Remove => unwanted items, runed EX

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,8 @@
 <script>
 
-//import { activeElement } from "runed";
 import  {Accordion}     from  'bits-ui';
 import  Dash            from  '$lib/Dash.svelte';
-import  {Button}        from   '$lib/components/ui/button';
-//import  '$lib/comp.svelte.js'
-////$effect(()=> console.log("Active element is: ", activeElement.current));
+import  {Button}        from  '$lib/components/ui/button';
 </script>
 
 <Dash />
-
-<Button> Hello </Button>
- 
-<Accordion.Root>
-    <Accordion.Item value="first">
-      <Accordion.Header>
-        <Accordion.Trigger>First</Accordion.Trigger>
-      </Accordion.Header>
-      <Accordion.Content>First accordion content</Accordion.Content>
-    </Accordion.Item>
-    <Accordion.Item value="second">
-      <Accordion.Header>
-        <Accordion.Trigger>Second</Accordion.Trigger>
-      </Accordion.Header>
-      <Accordion.Content>Second accordion content</Accordion.Content>
-    </Accordion.Item>
-    <Accordion.Item value="third">
-      <Accordion.Header>
-        <Accordion.Trigger>Third</Accordion.Trigger>
-      </Accordion.Header>
-      <Accordion.Content>Third accordion content</Accordion.Content>
-    </Accordion.Item>
-  </Accordion.Root>
-  
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
