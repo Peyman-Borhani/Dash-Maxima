6d45a407cbccf5fb812ebf691079e67a495c7450
peyman-borhani <peyman.borhani@gmail.com>
2024-08-12 13:33:50 +0330

    +Add: Search_Bar.svelte => moved from origin to  component

---
+++ src/lib/Search_Bar.svelte
@@ -0,0 +1,21 @@
+<script> 
+//Runes
+        import  Search        from  '~icons/lucide/search';
+        import  {Input}       from  '$lib/components/ui/input/index.js';
+
+        let  {inp_Elm,  activeElm}    = $props();
+        let  glow  = $derived(activeElm==='BODY'? ''  :'shadow-cyan-200');
+        $effect(()=>console.log(activeElm.current.nodeName))
+</script>
+
+
+<Search on class='flex  h-9 w-9 mr-5  p-1  text-muted-foreground'  />
+
+<div  class='fixed flex  md:grow-0  bg-red-500  h-15 w-full min-w-fit select-none'
+>
+    <Input  type  = 'search'
+            style = {glow? 'scale: 1.12;  box-shadow: 0 0 1ch #cde !important;'  :'opacity: 0;  width:1ch; border-radius: 50vmax;  border: solid 1pt #aaa'}
+            class = 'fixed placeholder-opacity-100 w-auto rounded-full bg-background pl-3 pr-2  md:w-[20ch] lg:w-[40ch]  text-md  scroll-m-10 opacity-60  transition-all duration-300'
+            placeholder={inp_Elm}
+    />
+  </div>
