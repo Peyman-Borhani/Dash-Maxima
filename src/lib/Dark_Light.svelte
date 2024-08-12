fb7013e027b5fcd2862e00675d983d6df9870922
peyman-borhani <peyman.borhani@gmail.com>
2024-08-12 13:29:51 +0330

    +Add: Component: Dark_Light.svelte => Dark/Light Theme toggle (using modeWatcher)

---
+++ src/lib/Dark_Light.svelte
@@ -0,0 +1,27 @@
+<script>
+  import  {setMode, mode, systemPrefersMode}  from  'mode-watcher';
+
+  setMode( $systemPrefersMode!=='light'? 'dark'  :'light' );
+  let Thm_Hov = false;
+</script>
+
+<button  class='flex rounded-full bg-transparent text-2xl select-none z-2 h-10 w-10 transition-all duration-1000'
+
+        onclick={()=> setMode($mode==='light'? 'dark' :'light')}
+        onpointerenter ={_=> Thm_Hov= true} 
+        onpointerleave ={_=> Thm_Hov= false}
+>   
+  
+        <span   class ='absolute  place-self-center font-semibold font-serif transition-all duration-700 -translate-x-4' style='line-height: 1.6ch; letter-spacing: -6pt;'
+              >   
+            {#if Thm_Hov} 🌜✨<br>🌞     
+                            <!--(  self-end -rotate-90 🙂✨🔆🌙 -->
+            {:else if  $mode==='light'}  🌞
+            {:else                    }  🌜✨     
+            {/if}
+        </span>
+     
+     <!-- <Moon  class ='text-purple-200 h-7 w-7'/> -->
+
+  <span  hidden> Theme "Dark/Light"  </span>
+</button>
