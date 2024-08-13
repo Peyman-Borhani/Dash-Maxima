694852580cfa90c5d1fd80bfffe46fe92afafabb
peyman-borhani <peyman.borhani@gmail.com>
2024-08-13 19:12:05 +0330

    +Add: Side_Bar.svelte => +moved from origin(Dash) +full code/ui update
    -Remove: unused items => abstracted into new components.

--- src/lib/Dash.svelte
+++ src/lib/Dash.svelte
@@ -1,4 +1,5 @@
 <script>
+//S5 Runes active
   import  ChevronLeft   from  '~icons/lucide/chevron-left';
   import  ChevronRight  from  '~icons/lucide/chevron-right';
   import  Copy          from  '~icons/lucide/copy';
@@ -10,174 +11,39 @@
   import  Package       from  '~icons/lucide/package';
   import  Package2      from  '~icons/lucide/package2';
   import  PanelLeft     from  '~icons/lucide/panel-left';
-  import  Search        from  '~icons/lucide/search';
-  import  Settings      from  '~icons/lucide/settings';
   import  ShoppingCart  from  '~icons/lucide/shopping-cart';
   import  Truck         from  '~icons/lucide/truck';
   import  UsersRound    from  '~icons/lucide/users-round';
-  import  Sun           from  '~icons/lucide/sun';
-  import  Moon          from  '~icons/lucide/moon';
   import  EllipsisVertical  from  '~icons/lucide/ellipsis-vertical';
-  import  {activeElement}   from  'runed';
    
   import  * as Breadcrumb     from '$lib/components/ui/breadcrumb/index.js';
   import  * as Card           from '$lib/components/ui/card/index.js';
   import  * as DropdownMenu   from '$lib/components/ui/dropdown-menu/index.js';
   import  * as Pagination     from '$lib/components/ui/pagination/index.js';
-  import  * as Tooltip        from '$lib/components/ui/tooltip/index.js';
   import  {Badge}       from '$lib/components/ui/badge/index.js';
   import  {Button}      from '$lib/components/ui/button/index.js';
-  import  {Input}       from '$lib/components/ui/input/index.js';
   import  {Progress}    from '$lib/components/ui/progress/index.js';
   import  {Separator}   from '$lib/components/ui/separator/index.js';
   import  * as Sheet    from '$lib/components/ui/sheet/index.js';
   import  * as Table    from '$lib/components/ui/table/index.js';
   import  * as Tabs     from '$lib/components/ui/tabs/index.js';
-  import  {setMode, mode, systemPrefersMode}  from  'mode-watcher';
 
-    $effect(()=> console.log(activeElement.current.nodeName))
-    let inp_Elm =$derived( (activeElement.current?.nodeName==='INPUT'? 'Search...' 
-                          : activeElement.current?.nodeName!=='BODY'?  activeElement.current?.textContent
-                          : 'Search... or hit interactive elements'
-    ) );
-    let glow    =$derived(activeElement.current?.nodeName==='BODY'? ''  :'shadow-cyan-200');
-    setMode( $systemPrefersMode !== 'light'?  'dark'  :'light' );
+  import  Dark_Light        from  '$lib/Dark_Light.svelte';
+  import  Side_Bar          from  '$lib/Side_Bar.svelte';
+  import  Search_Bar        from  '$lib/Search_Bar.svelte';
+  import  {activeElement}   from  'runed';
+
+
+//$effect( _=> console.log(activeElement.current?.nodeName) );
+
+let  inp_Elm =$derived( (activeElement.current?.nodeName==='INPUT'? 'Search...' 
+                        : activeElement.current?.nodeName!=='BODY'?  activeElement.current?.textContent
+                        : 'Doc Body - Search or click interactive items'
+) );
 
 </script>
   
-  
-  <div class='flex min-h-screen w-full flex-col bg-muted/40'>
-    <aside class='fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex'>
-      <nav class='flex flex-col items-center gap-4 px-2 sm:py-5 group-hover:scale-125'>
-        <a
-          href='##'
-          class='flex h-9 w-9  group shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
-        >
-          <Package2 class='h-4 w-4  group-hover:scale-125  transition-all duration-300' />
-          <span class='sr-only'>Acme Inc</span>
-        </a>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href='##'
-              class='group flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
-              use:builder.action
-              {...builder}
-            >
-              <Home class='h-5 w-5 group-hover:scale-125  transition-all duration-300' />
-              <span class='sr-only'>Dashboard</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side='right'>Dashboard</Tooltip.Content>
-        </Tooltip.Root>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href='##'
-              class='flex h-9 w-9  group items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
-              use:builder.action
-              {...builder}
-            >
-              <ShoppingCart class='h-5 w-5  group-hover:scale-125  transition-all duration-300' />
-              <span class='sr-only'>Orders</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side='right'>Orders</Tooltip.Content>
-        </Tooltip.Root>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href='##'
-              class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
-              use:builder.action
-              {...builder}
-            >
-              <Package class='h-5 w-5  group-hover:scale-125  transition-all duration-300' />
-              <span class='sr-only'>Products</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side='right'>Products</Tooltip.Content>
-        </Tooltip.Root>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href='##'
-              class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
-              use:builder.action
-              {...builder}
-            >
-              <UsersRound class='h-5 w-5  group-hover:scale-125  transition-all duration-300' />
-              <span class='sr-only'>Customers</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side='right'>Customers</Tooltip.Content>
-        </Tooltip.Root>
-        
-        <Tooltip.Root>
-        <Tooltip.Trigger asChild let:builder>
-            <a
-              href='##'
-              class='flex h-9 w-9  group items-center justify-center rounded-lg text-cyan-300 hover:text-foreground text-xl md:h-8 md:w-8'
-              use:builder.action
-              {...builder}
-            >
-              <p    class='group-hover:scale-125  transition-all duration-300' 
-                    style='text-shadow: 0 0 1ch #def;'>
-                    {activeElement.current?.tagName[0]}
-              </p>
-              <span style='color: red !important;' class='sr-only'>{activeElement?.nodeName}</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side='right'>{inp_Elm}</Tooltip.Content>
-        </Tooltip.Root>
-
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href='##'
-              class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
-              use:builder.action
-              {...builder}
-            >
-              <LineChart class='h-5 w-5  group-hover:scale-125  transition-all duration-300' />
-              <span class='sr-only'>Analytics</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side='right'>Analytics</Tooltip.Content>
-        </Tooltip.Root>
-      </nav>
-      <nav class='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href='##'
-              class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
-              use:builder.action
-              {...builder}
-            >
-              <Settings class='h-5 w-5  group-hover:scale-125  transition-all duration-300' />
-              <span class='sr-only'>Settings</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side='right'>Settings</Tooltip.Content>
-        </Tooltip.Root>
-      </nav>
-    </aside>
-    <div class='flex flex-col sm:gap-4 sm:py-4 sm:pl-14'>
-      <header
-        class='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'
-      >
-        <Sheet.Root>
-          <Sheet.Trigger asChild let:builder>
-            <Button builders={[builder]} size='icon' variant='outline' class='sm:hidden'>
-              <PanelLeft class='h-5 w-5' />
-              <span class='sr-only'>Toggle Menu</span>
-            </Button>
-          </Sheet.Trigger>
-          <Sheet.Content side='left' class='sm:max-w-xs'>
-            <nav class='grid gap-6 text-lg font-medium'>
-              <a
-                href='##'
+<div  class='flex min-h-screen w-full flex-col bg-muted/40'>
                 class='flex h-10 w-10 group shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base'
               >
                 <Package2 class='h-5 w-5 transition-all  group-hover:scale-110' />
---
+++ src/lib/Side_Bar.svelte
@@ -0,0 +1,134 @@
+<script>
+    import  Home          from  '~icons/lucide/home';
+    import  LineChart     from  '~icons/lucide/line-chart';
+    import  Package       from  '~icons/lucide/package';
+    import  Package2      from  '~icons/lucide/package2';
+    import  UsersRound    from  '~icons/lucide/users-round';
+    import  Settings      from  '~icons/lucide/settings';
+    import  ShoppingCart  from  '~icons/lucide/shopping-cart';
+
+    import  * as Tooltip  from '$lib/components/ui/tooltip/index.js';
+    import  {activeElement}   from  'runed';
+
+    let  {inp_Elm}    = $props();
+
+</script>
+
+<aside class='fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex'>
+    <nav  class='flex flex-col items-center gap-6 px-8 sm:py-5  group-hover:scale-150'>
+      <a  href='##'
+        class='flex h-9 w-9  group shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
+      >
+        <Package2  class='h-4 w-4  group-hover:scale-150  transition-all duration-300' />
+        <span  class='sr-only'> Systemic </span>
+      </a>
+
+      <Tooltip.Root >
+        <Tooltip.Trigger  asChild  let:builder class ='text-2xl'>
+          <a
+            href='##'
+            class='group flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+            use:builder.action
+            {...builder}
+          >
+            <Home  class='h-5 w-5 group-hover:scale-150  transition-all duration-300' />
+            <span  class='sr-only'>Dashboard</span>
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side='right'>Dashboard</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <a
+            href='##'
+            class='flex h-9 w-9  group items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+            use:builder.action
+            {...builder}
+          >
+            <ShoppingCart class='h-5 w-5  group-hover:scale-150  transition-all duration-300' />
+            <span class='sr-only'>Orders</span>
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side='right'>Orders</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <a
+            href='##'
+            class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+            use:builder.action
+            {...builder}
+          >
+            <Package class='h-5 w-5  group-hover:scale-150  transition-all duration-300' />
+            <span class='sr-only'>Products</span>
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side='right'>Products</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <a
+            href='##'
+            class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+            use:builder.action
+            {...builder}
+          >
+            <UsersRound class='h-5 w-5  group-hover:scale-150  transition-all duration-300' />
+            <span class='sr-only'>Customers</span>
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side='right'>Customers</Tooltip.Content>
+      </Tooltip.Root>
+      
+      <Tooltip.Root>
+      <Tooltip.Trigger asChild let:builder>
+          <a
+            href='##'
+            class='flex h-9 w-9  group items-center justify-center rounded-lg text-cyan-300 hover:text-foreground text-xl md:h-8 md:w-8'
+            use:builder.action
+            {...builder}
+          >
+            <p    class='group-hover:scale-150  transition-all duration-300 font-sans' 
+                  style='text-shadow: 0 0 1ch #def;  letter-spacing: -2pt'>
+                  EL
+            </p>
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side='right' class='ml-2 p-4 text-md font-semibold text-violet-600'> Tag-name: 
+            <span class='font-bold text-lg text-green-500'> {activeElement.current?.nodeName} </span> &nbsp - &nbsp Item: 
+            <span class='font-bold text-lg italic text-green-500'> {inp_Elm} </span>
+        </Tooltip.Content>
+      </Tooltip.Root>
+
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <a
+            href='##'
+            class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+            use:builder.action
+            {...builder}
+          >
+            <LineChart class='h-5 w-5  group-hover:scale-150  transition-all duration-300' />
+            <span class='sr-only'>Analytics</span>
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side='right'>Analytics</Tooltip.Content>
+      </Tooltip.Root>
+    </nav>
+    <nav class='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <a
+            href='##'
+            class='flex h-9 w-9  group items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+            use:builder.action
+            {...builder}
+          >
+            <Settings class='h-5 w-5  group-hover:scale-150  transition-all duration-300' />
+            <span class='sr-only'>Settings</span>
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side='right'>Settings</Tooltip.Content>
+      </Tooltip.Root>
+    </nav>
+</aside>
