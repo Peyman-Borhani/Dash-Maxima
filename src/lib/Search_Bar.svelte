4929b69e589a13d2aab029d22b9832c682ed1922
peyman-borhani <peyman.borhani@gmail.com>
2024-08-14 15:10:56 +0330

    +Fixed: Search_Bar component => all issues

--- src/lib/Search_Bar.svelte
+++ src/lib/Search_Bar.svelte
@@ -1,20 +1,34 @@
 <script> 
 //Runes
-        import  Search        from  '~icons/lucide/search';
-        import  {Input}       from  '$lib/components/ui/input/index.js';
+        import  Search      from  '~icons/lucide/search';
+        import  {Input}     from  '$lib/components/ui/input/index.js';
 
-        let  {inp_Elm,  activeElm}    = $props();
-        let  glow  = $derived(activeElm==='BODY'? ''  :'shadow-cyan-200');
+        let  {inp_Elm}  = $props();
+        let  view  = $state(false);
 </script>
 
-
-<Search on class='flex  h-9 w-9 mr-5  p-1  text-muted-foreground'  />
-
-<div  class='fixed flex  md:grow-0  bg-red-500  h-15 w-full min-w-fit select-none'
+<div  class ='fixed  inline-grid  right-[14vw]  md:grow-0  h-15  min-w-fit 
+              select-none  drop-shadow-lg  shadow-cyan-700  overflow-hidden'
 >
-    <Input  type  = 'search'
-            style = {glow? 'scale: 1.12;  box-shadow: 0 0 1ch #cde !important;'  :'opacity: 0;  width:1ch; border-radius: 50vmax;  border: solid 1pt #aaa'}
-            class = 'fixed placeholder-opacity-100 w-auto rounded-full bg-background pl-3 pr-2  md:w-[20ch] lg:w-[40ch]  text-md  scroll-m-10 opacity-60  transition-all duration-300'
-            placeholder={inp_Elm}
-    />
-  </div>
+  <Input  type  ='search'
+          style ={view? 'display: inline-grid;'  : 'display:none;'}
+          class ='placeholder-opacity-100 w-auto rounded-full  pl-3  pr-2  bg-background
+                  md:w-[20ch] lg:w-[40ch]  text-md  scroll-m-90  transition-all duration-300'
+          placeholder={inp_Elm}
+  />
+      
+  {#if view}
+    <button   onpointerdown={_=> view=false}
+              class='absolute px-2 py-1  scale-x-125 scale-y-105 text-2xl font-thin bg-violet-900  text-white mr-1 justify-self-end self-center rounded-l-md rounded-r-full   '
+            > X
+    </button>
+  {/if}
+</div>
+
+<button >
+  <Search  onpointerdown={()=> view=true} 
+            class='flex  h-9 w-9 mr-10  p-1  text-muted-foreground'
+  />
+  <span class=sr-only>Search...</span>
+</button>
+    <!--style = {glow? 'scale: 1.12;  box-shadow: 0 0 1ch #cde !important;'  :'opacity: 0;  width:1ch; border-radius: 50vmax;  border: solid 1pt #aaa'} -->
