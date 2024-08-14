eefa57165ebc38d8c039e56440e219e664443f33
peyman-borhani <peyman.borhani@gmail.com>
2024-08-14 17:31:57 +0330

    +Improve: Search_Bar => styling, function

--- src/lib/Search_Bar.svelte
+++ src/lib/Search_Bar.svelte
@@ -7,28 +7,29 @@
         let  view  = $state(false);
 </script>
 
+<button  onclick ={()=> view=true}>
+  <Search  class='flex  h-9 w-9 mr-10  p-1  text-muted-foreground'
+  />
+  <span class=sr-only>Search...</span>
+</button>
+
 <div  class ='fixed  inline-grid  right-[14vw]  md:grow-0  h-15  min-w-fit 
-              select-none  drop-shadow-lg  shadow-cyan-700  overflow-hidden'
+              select-none  drop-shadow-lg  shadow-cyan-700'
 >
   <Input  type  ='search'
-          style ={view? 'display: inline-grid;'  : 'display:none;'}
-          class ='placeholder-opacity-100 w-auto rounded-full  pl-3  pr-2  bg-background
-                  md:w-[20ch] lg:w-[40ch]  text-md  scroll-m-90  transition-all duration-300'
+          style ={view? 'display: inline-grid; box-shadow: 0 0 1.7ch #306'  : 'display:none;'}
+          class ='placeholder-opacity-100 w-auto rounded-full  px-2  bg-background
+                  md:w-[20ch] lg:w-[40ch]  text-xl  scroll-m-90  transition-all duration-400'
           placeholder={inp_Elm}
   />
       
   {#if view}
-    <button   onpointerdown={_=> view=false}
-              class='absolute px-2 py-1  scale-x-125 scale-y-105 text-2xl font-thin bg-violet-900  text-white mr-1 justify-self-end self-center rounded-l-md rounded-r-full   '
+    <button   onpointerdown={()=> view=false}
+              class='absolute px-2 py-1 scale-x-125 text-2xl font-normal text-[#61c]
+                     justify-self-end self-center rounded-l-3xl rounded-r-full  shadow-inner  shadow-violet-900  bg-background'
             > X
     </button>
   {/if}
 </div>
 
-<button >
-  <Search  onpointerdown={()=> view=true} 
-            class='flex  h-9 w-9 mr-10  p-1  text-muted-foreground'
-  />
-  <span class=sr-only>Search...</span>
-</button>
     <!--style = {glow? 'scale: 1.12;  box-shadow: 0 0 1ch #cde !important;'  :'opacity: 0;  width:1ch; border-radius: 50vmax;  border: solid 1pt #aaa'} -->
