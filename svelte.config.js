a32d7a3008bdf05e637df7d00a534515539c5dc0
peyman-borhani <peyman.borhani@gmail.com>
2024-08-16 09:56:41 +0330

    ~Deploy Solving: config => add prerender

--- svelte.config.js
+++ svelte.config.js
@@ -10,12 +10,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-        alias: {
-            "@/*": "./src/lib/*",
-           // "icns": "node_modules/lucide-svelte/dist/icons",
-           // "icns/*": "node_modules/lucide-svelte/dist/icons/*"
-        },
-        
+        prerender: {handleHttpError: ({ path, referrer, message })=> {return}} ,
+        alias: {"@/*": "./src/lib/*",
+        },        
 	}
 };
 
