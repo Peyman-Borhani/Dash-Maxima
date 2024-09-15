6e838dded22cdc0ecf047dd9841d2ee4149bfd4c
peyman-borhani <peyman.borhani@gmail.com>
2024-09-15 14:49:45 +0330

    +Changing: +>Add: >Content => grid CSS +Improve: portrait styles ~Refactor styles ~Restructure-content

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -21,13 +21,13 @@
 </script>
 
 
-<div class='grid   w-full  items-start   justify-between  
-            auto-rows-max  lg:col-span-2  gap-x-2  gap-y-[6svh] '
+<div class='grid   w-full  items-start   justify-between grid-cols-6
+            auto-rows-max  col-span-full  gap-x-[2svw]  gap-y-[8svh] min-h-full'
 >
 
-  <div class='grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4'>
+  <div class='grid gap-4 col-span-5 row-span-2  xs:grid-cols-1 sm:grid-cols-3  md-grid-cols-3 landscape:grid-cols-5 '>
     
-    <Card.Root class='sm:col-span-2'>
+    <Card.Root class='col-span-4 row-span-1 landscape:col-span-2 '>
         <Card.Header class='pb-4'>
             <Card.Title>Your Orders</Card.Title>
             <Card.Description class='max-w-lg text-balance leading-relaxed'>
@@ -39,7 +39,7 @@
         </Card.Footer>
     </Card.Root>
 
-    <Card.Root>
+    <Card.Root  class='col-span-1 row-span-1 row-start-2 '>
         <Card.Header class='pb-2'>
             <Card.Description>This Week</Card.Description>
             <Card.Title class='text-4xl'>$1329</Card.Title>
@@ -52,7 +52,7 @@
         </Card.Footer>
     </Card.Root>
         
-    <Card.Root>
+    <Card.Root  class='col-span-1 row-span-1'>
         <Card.Header class='pb-2'>
             <Card.Description>This Month</Card.Description>
             <Card.Title class='text-3xl'>$5,329</Card.Title>
@@ -66,6 +66,165 @@
     </Card.Root>
   </div>
 
+  <div class = 'portrait:col-span-full  portrait:col-start-1  portrait:row-start-3  landscape:row-start-1 landscape:col-start-5 landscape:col-span-2 landscape:row-span-3' >
+    <Card.Root class='overflow-hidden'>
+    <Card.Header class='flex flex-row items-start bg-muted/50'>
+        <div class='grid gap-0.5'>
+        <Card.Title  class='group flex items-center gap-2 text-lg'
+                        onclick = {e=> navigator.clipboard.writeText(e.currentTarget.childNodes[1].textContent)}
+        > Order Oe31b70H
+            <Button
+                size    ='icon'
+                variant ='outline'
+                class   ='h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100'
+            >
+            <Copy class='size-3 scale-y-125' />
+            <span class='sr-only'>Copy Order ID</span>
+            </Button>
+        </Card.Title>
+        <Card.Description>Date: November 23, 2023</Card.Description>
+        </div>
+        <div class='ml-auto flex items-center gap-1'>
+        <Button size='sm' variant='outline' class='h-8 gap-1'>
+            <Truck class='h-3.5 w-3.5' />
+            <span class='lg:sr-only xl:not-sr-only xl:whitespace-nowrap'>
+            Track Order
+            </span>
+        </Button>
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild let:builder>
+            <Button
+                builders={[builder]}
+                size='icon'
+                variant='outline'
+                class='h-8 w-8'
+            >
+                <EllipsisVertical class='h-3.5 w-3.5' />
+                <span class='sr-only'>More</span>
+            </Button>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content align='end'>
+            <DropdownMenu.Item>Edit</DropdownMenu.Item>
+            <DropdownMenu.Item>Export</DropdownMenu.Item>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item>Trash</DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
+        </div>
+    </Card.Header>
+    <Card.Content class='p-6 text-sm'>
+        <div class='grid gap-3'>
+        <div class='font-semibold'>Order Details</div>
+        <ul class='grid gap-3'>
+            <li class='flex items-center justify-between'>
+            <span class='text-muted-foreground'>
+                Glimmer Lamps x <span>2</span>
+            </span>
+            <span>$250.00</span>
+            </li>
+            <li class='flex items-center justify-between'>
+            <span class='text-muted-foreground'>
+                Aqua Filters x <span>1</span>
+            </span>
+            <span>$49.00</span>
+            </li>
+        </ul>
+        <Separator class='my-2' />
+        <ul class='grid gap-3'>
+            <li class='flex items-center justify-between'>
+            <span class='text-muted-foreground'>Subtotal</span>
+            <span>$299.00</span>
+            </li>
+            <li class='flex items-center justify-between'>
+            <span class='text-muted-foreground'>Shipping</span>
+            <span>$5.00</span>
+            </li>
+            <li class='flex items-center justify-between'>
+            <span class='text-muted-foreground'>Tax</span>
+            <span>$25.00</span>
+            </li>
+            <li class='flex items-center justify-between font-semibold'>
+            <span class='text-muted-foreground'>Total</span>
+            <span>$329.00</span>
+            </li>
+        </ul>
+        </div>
+        <Separator class='my-4' />
+        <div class='grid grid-cols-2 gap-4'>
+        <div class='grid gap-3'>
+            <div class='font-semibold'>Shipping Information</div>
+            <address class='grid gap-0.5 not-italic text-muted-foreground'>
+            <span>Liam Johnson</span>
+            <span>1234 Main St.</span>
+            <span>Anytown, CA 12345</span>
+            </address>
+        </div>
+        <div class='grid auto-rows-max gap-3'>
+            <div class='font-semibold'>Billing Information</div>
+            <div class='text-muted-foreground'>Same as shipping address</div>
+        </div>
+        </div>
+        <Separator class='my-4' />
+        <div class='grid gap-3'>
+        <div class='font-semibold'>Customer Information</div>
+        <dl class='grid gap-3'>
+            <div class='flex items-center justify-between'>
+            <dt class='text-muted-foreground'>Customer</dt>
+            <dd>Liam Johnson</dd>
+            </div>
+            <div class='flex items-center justify-between'>
+            <dt class='text-muted-foreground'>Email</dt>
+            <dd>
+                <a href='mailto:'>liam@acme.com</a>
+            </dd>
+            </div>
+            <div class='flex items-center justify-between'>
+            <dt class='text-muted-foreground'>Phone</dt>
+            <dd>
+                <a href='tel:'>+1 234 567 890</a>
+            </dd>
+            </div>
+        </dl>
+        </div>
+        <Separator class='my-4' />
+        <div class='grid gap-3'>
+        <div class='font-semibold'>Payment Information</div>
+        <dl class='grid gap-3'>
+            <div class='flex items-center justify-between'>
+            <dt class='flex items-center gap-1 text-muted-foreground'>
+                <CreditCard class='h-4 w-4' />
+                Visa
+            </dt>
+            <dd>**** **** **** 4532</dd>
+            </div>
+        </dl>
+        </div>
+    </Card.Content>
+    <Card.Footer class='flex flex-row items-center border-t bg-muted/50 px-6 py-3'>
+        <div class='text-xs text-muted-foreground'>
+        Updated <time dateTime='2023-11-23'>November 23, 2023</time>
+        </div>
+        <Pagination.Root count={10} class='ml-auto mr-0 w-auto'>
+        <Pagination.Content>
+            <Pagination.Item>
+            <Button size='icon' variant='outline' class='h-6 w-6'>
+                <ChevronLeft class='h-3.5 w-3.5' />
+                <span class='sr-only'>Previous Order</span>
+            </Button>
+            </Pagination.Item>
+            <Pagination.Item>
+            <Button size='icon' variant='outline' class='h-6 w-6'>
+                <ChevronRight class='h-3.5 w-3.5' />
+                <span class='sr-only'>Next Order</span>
+            </Button>
+            </Pagination.Item>
+        </Pagination.Content>
+        </Pagination.Root>
+    </Card.Footer>
+    </Card.Root>
+  </div>
+
+ <div class = 'row-start-4 col-span-full'>
   <Tabs.Root  value='week'>
     <div class='flex items-center'>
         <Tabs.List>
@@ -102,7 +261,7 @@
         </Button>
       </div>
     </div>
-
+    
         <Tabs.Content value='week'>
             <Card.Root>
             <Card.Header class='px-7'>
@@ -309,162 +468,5 @@
             </Card.Root>
         </Tabs.Content>
     </Tabs.Root>
-</div>
-
-    <div>
-        <Card.Root class='overflow-hidden'>
-        <Card.Header class='flex flex-row items-start bg-muted/50'>
-            <div class='grid gap-0.5'>
-            <Card.Title  class='group flex items-center gap-2 text-lg'
-                            onclick = {e=> navigator.clipboard.writeText(e.currentTarget.childNodes[1].textContent)}
-            > Order Oe31b70H
-                <Button
-                    size    ='icon'
-                    variant ='outline'
-                    class   ='h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100'
-                >
-                <Copy class='size-3 scale-y-125' />
-                <span class='sr-only'>Copy Order ID</span>
-                </Button>
-            </Card.Title>
-            <Card.Description>Date: November 23, 2023</Card.Description>
-            </div>
-            <div class='ml-auto flex items-center gap-1'>
-            <Button size='sm' variant='outline' class='h-8 gap-1'>
-                <Truck class='h-3.5 w-3.5' />
-                <span class='lg:sr-only xl:not-sr-only xl:whitespace-nowrap'>
-                Track Order
-                </span>
-            </Button>
-            <DropdownMenu.Root>
-                <DropdownMenu.Trigger asChild let:builder>
-                <Button
-                    builders={[builder]}
-                    size='icon'
-                    variant='outline'
-                    class='h-8 w-8'
-                >
-                    <EllipsisVertical class='h-3.5 w-3.5' />
-                    <span class='sr-only'>More</span>
-                </Button>
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content align='end'>
-                <DropdownMenu.Item>Edit</DropdownMenu.Item>
-                <DropdownMenu.Item>Export</DropdownMenu.Item>
-                <DropdownMenu.Separator />
-                <DropdownMenu.Item>Trash</DropdownMenu.Item>
-                </DropdownMenu.Content>
-            </DropdownMenu.Root>
-            </div>
-        </Card.Header>
-        <Card.Content class='p-6 text-sm'>
-            <div class='grid gap-3'>
-            <div class='font-semibold'>Order Details</div>
-            <ul class='grid gap-3'>
-                <li class='flex items-center justify-between'>
-                <span class='text-muted-foreground'>
-                    Glimmer Lamps x <span>2</span>
-                </span>
-                <span>$250.00</span>
-                </li>
-                <li class='flex items-center justify-between'>
-                <span class='text-muted-foreground'>
-                    Aqua Filters x <span>1</span>
-                </span>
-                <span>$49.00</span>
-                </li>
-            </ul>
-            <Separator class='my-2' />
-            <ul class='grid gap-3'>
-                <li class='flex items-center justify-between'>
-                <span class='text-muted-foreground'>Subtotal</span>
-                <span>$299.00</span>
-                </li>
-                <li class='flex items-center justify-between'>
-                <span class='text-muted-foreground'>Shipping</span>
-                <span>$5.00</span>
-                </li>
-                <li class='flex items-center justify-between'>
-                <span class='text-muted-foreground'>Tax</span>
-                <span>$25.00</span>
-                </li>
-                <li class='flex items-center justify-between font-semibold'>
-                <span class='text-muted-foreground'>Total</span>
-                <span>$329.00</span>
-                </li>
-            </ul>
-            </div>
-            <Separator class='my-4' />
-            <div class='grid grid-cols-2 gap-4'>
-            <div class='grid gap-3'>
-                <div class='font-semibold'>Shipping Information</div>
-                <address class='grid gap-0.5 not-italic text-muted-foreground'>
-                <span>Liam Johnson</span>
-                <span>1234 Main St.</span>
-                <span>Anytown, CA 12345</span>
-                </address>
-            </div>
-            <div class='grid auto-rows-max gap-3'>
-                <div class='font-semibold'>Billing Information</div>
-                <div class='text-muted-foreground'>Same as shipping address</div>
-            </div>
-            </div>
-            <Separator class='my-4' />
-            <div class='grid gap-3'>
-            <div class='font-semibold'>Customer Information</div>
-            <dl class='grid gap-3'>
-                <div class='flex items-center justify-between'>
-                <dt class='text-muted-foreground'>Customer</dt>
-                <dd>Liam Johnson</dd>
-                </div>
-                <div class='flex items-center justify-between'>
-                <dt class='text-muted-foreground'>Email</dt>
-                <dd>
-                    <a href='mailto:'>liam@acme.com</a>
-                </dd>
-                </div>
-                <div class='flex items-center justify-between'>
-                <dt class='text-muted-foreground'>Phone</dt>
-                <dd>
-                    <a href='tel:'>+1 234 567 890</a>
-                </dd>
-                </div>
-            </dl>
-            </div>
-            <Separator class='my-4' />
-            <div class='grid gap-3'>
-            <div class='font-semibold'>Payment Information</div>
-            <dl class='grid gap-3'>
-                <div class='flex items-center justify-between'>
-                <dt class='flex items-center gap-1 text-muted-foreground'>
-                    <CreditCard class='h-4 w-4' />
-                    Visa
-                </dt>
-                <dd>**** **** **** 4532</dd>
-                </div>
-            </dl>
-            </div>
-        </Card.Content>
-        <Card.Footer class='flex flex-row items-center border-t bg-muted/50 px-6 py-3'>
-            <div class='text-xs text-muted-foreground'>
-            Updated <time dateTime='2023-11-23'>November 23, 2023</time>
-            </div>
-            <Pagination.Root count={10} class='ml-auto mr-0 w-auto'>
-            <Pagination.Content>
-                <Pagination.Item>
-                <Button size='icon' variant='outline' class='h-6 w-6'>
-                    <ChevronLeft class='h-3.5 w-3.5' />
-                    <span class='sr-only'>Previous Order</span>
-                </Button>
-                </Pagination.Item>
-                <Pagination.Item>
-                <Button size='icon' variant='outline' class='h-6 w-6'>
-                    <ChevronRight class='h-3.5 w-3.5' />
-                    <span class='sr-only'>Next Order</span>
-                </Button>
-                </Pagination.Item>
-            </Pagination.Content>
-            </Pagination.Root>
-        </Card.Footer>
-        </Card.Root>
+  </div>
 </div>
