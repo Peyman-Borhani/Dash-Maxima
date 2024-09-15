114adecd865b5a16034366c714c37d6074e4305e
peyman-borhani <peyman.borhani@gmail.com>
2024-09-15 14:47:05 +0330

    -remove: non-used styles +improve-fix-refactor: styles

--- src/lib/Dash.svelte
+++ src/lib/Dash.svelte
@@ -16,8 +16,8 @@ let  Active_Itm =$derived(
 </script>
   
 
-<span   class ='grid   w-[100svw]    h-[100svh]    p-0 m-0  place-self-stretch  place-content-evenly overflow-x-hidden
-                fixed  grid-rows-12  grid-cols-12  text-[calc((1ch + 2vmin + 1vw) / 3)] bg-muted/75 '
+<span   class ='grid  fixed   w-svw  h-svh  p-0 m-0 z-0  place-self-stretch  place-content-evenly    
+                grid-rows-12  grid-cols-12  bg-muted/75  overflow-x-hidden   text-[calc((1ch + 2vmin + 1vw) / 3)] '
         
 >
 
@@ -26,8 +26,8 @@ let  Active_Itm =$derived(
                  menu <-(force menu)    -  fixed (placement/won't shift)   
                  {Active_Itm} (focused element)
     -->
-    <header  class ='inline-grid   portrait:fixed   grid-cols-12  col-start-1  -col-end-1  
-                    items-center   justify-center   row-start-1   w-full  border-x-2  border-muted  '
+    <header  class ='sub-grid   portrait:fixed   col-start-1  col-end-12  row-start-1
+                     items-center   justify-center   w-full  border-x-2   border-muted  '
     >
         <Header  fixed />  <!--   props:  fixed (stays, won't scroll)  
         Header contains_____________________________________________
@@ -37,12 +37,12 @@ let  Active_Itm =$derived(
         <Search_Bar  {Active_Itm}  />    
     </header>
 
-    <main   class ='grid   col-start-2  col-span-10 row-start-3 row-end-auto auto-rows-auto  h-fit rounded-lg
-                    gap-8  grid-cols-1  lg:grid-cols-2  xl:grid-cols-3  landscape:grid-cols-3  p-4 m-0  bg-black/25'
+    <main   class ='grid  h-fit  col-start-2  col-end-12   col-span-10  portrait:-col-end-1 rounded-lg 
+                    p-4   ml-4   row-start-3  row-end-auto auto-rows-auto bg-black/25  portrait:row-start-2  portrait:mr-10'
             style ='box-shadow: 0 0 1ch #000;'
     >       
-                <Content />
-    </main> 
+            <Content />
+    </main>
   
 </span>
   
