34432d3bb518d5cdc2aa293ffcb473e166ff3add
peyman-borhani <peyman.borhani@gmail.com>
2024-11-01 20:18:49 +0330

    +Update: Header => fix props +Add: Fullscreen, Search_Bar +Add: Interactive Breadcrumb items

--- src/lib/Dark_Light.svelte
+++ src/lib/Dark_Light.svelte
@@ -1,14 +1,14 @@
 <script>
-  import  {setMode, mode, systemPrefersMode}  from  'mode-watcher';
-  //import    Sun     from  '~icons/system-uicons/moon';
-  import    Sun     from  '~icons/f7/sun-max-fill';
-  import    Moon    from  '~icons/radix-icons/moon';
-
-  setMode( $systemPrefersMode==='dark'?  'dark'  :'light' );
-  let Thm_Hov = $state(false);
- // let  {onpointerenter, onpointerleave, onpointerup} = $props();{onpointerup} {onpointerenter} {onpointerleave}
+    import  {setMode, mode, systemPrefersMode}  from  'mode-watcher';
+    //import    Sun     from  '~icons/system-uicons/moon';
+    import    Sun     from  '~icons/f7/sun-max-fill';
+    import    Moon    from  '~icons/radix-icons/moon';
+  
+    setMode( $systemPrefersMode==='dark'?  'dark'  :'light' );
+    let Thm_Hov = $state(false);
+   // let  {onpointerenter, onpointerleave, onpointerup} = $props();{onpointerup} {onpointerenter} {onpointerleave}
 </script>
-
+  
 <button   
         class ='inline-grid  z-30  size-9  text-3xl  self-center  place-items-center select-none rounded-full col-start-9
                 shadow-inner shadow-violet-700  transition-all duration-300 dark:text-violet-300/80'
@@ -16,14 +16,11 @@
         onpointerup     ={()=> {setMode($mode==='light'? 'dark' :'light'); Thm_Hov=false} }
         onpointerenter  ={_=> Thm_Hov= true} 
         onpointerleave  ={_=> Thm_Hov= false}
->   
-                      <!--in case of emoji mix:🙂🌙🔆🌜✨🌞 -rotate-90 -->
-            {#if  $mode==='light'}  <Sun  class='bg-yellow-100  rounded-full shadow-lg shadow-yellow-300' />
-            {:else}                 <Moon class= 'p-0.5 hover:border border-violet-600 rounded-full shadow-inner shadow-violet-600 ' />
-            {/if}
+>                               <!--in case of emoji mix:🙂🌙🔆🌜✨🌞 -rotate-90 -->
+        {#if  $mode==='light'}  <Sun  class='bg-yellow-100  rounded-full shadow-lg shadow-yellow-300' />
+        {:else}                 <Moon class= 'p-0.5 hover:border border-violet-600 rounded-full shadow-inner shadow-violet-600 ' />
+        {/if}
 
-     
-     <!-- <Moon  class ='text-purple-200 h-7 w-7'/> -->
-
-  <span  hidden> Theme "Dark/Light"  </span>
+       <!-- <Moon  class ='text-purple-200 h-7 w-7'/> -->
+    <span  hidden> Theme "Dark/Light"  </span>
 </button>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,67 +1,85 @@
 <script>
     
-  import    * as Breadcrumb     from '$lib/components/ui/breadcrumb/index.js';
-  import    * as DropdownMenu   from '$lib/components/ui/dropdown-menu/index.js';
-  import    Dark_Light          from '$lib/Dark_Light.svelte';
-  import    {Button}            from '$lib/components/ui/button/index.js';
-
+  import    * as Breadcrumb     from  '$lib/components/ui/breadcrumb/index.js'
+  import    * as DropdownMenu   from  '$lib/components/ui/dropdown-menu/index.js'
+  import    Dark_Light          from  '$lib/Dark_Light.svelte'
+  import    Search_Bar          from  '$lib/Search_Bar.svelte'
+  import    {Button}            from  '$lib/components/ui/button/index.js'
+//import    Fullscreen          from  '~icons/radix-icons/enter-full-screen'
+  import    Fullscreen          from  '~icons/lucide/fullscreen'
   
-  //inline-grid  top-0  w-full  h-24  //items-center  content-center self-start  justify-center  sm:gap-4  rounded-b-md'
-    let {fixed}  =$props();
+  
+  let   {cfg,   Active_Itm=$bindable(''),     On_Page=$bindable('Home'),
+         items= ['Home', 'Orders', 'Products', 'Customers', 'Analytics', '', 'Settings']
+  }=$props();     //max word size among items. min: 2 (char+icon) //const menu_size = `${items.reduce((x,On_Page)=> {return On_Page.length>x? On_Page.length :x}, 2)}ch`;
+      
+  let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
+   
+  //just Content - (no Side_Bar, no Header...)
+  //let  None  =$derived(!P_sbar && !H_bar && !F_bar); 
+  
+  //const itm  = $derived.by(()=>{ (Active_Itm?.slice(0,2)==='# '? Active_Itm?.slice(2,Active_Itm?.length-2) :Active_Itm[0]==='#'? Active_Itm?.slice(1,Active_Itm.length-1)  :On_Page) })  ;
+  let itm = $derived.by(()=>((On_Page!=='Home' && Active_Itm[0]!=='#' && typeof Active_Itm==='string')? Active_Itm  :null) );
 </script>
-
-
-<span   class =
-      ' inline-grid   m-0 top-0  h-fit w-full {fixed? 'fixed ' :'absolute '}
-        grid-cols-12  p-0 z-30  min-h-[8ch]  items-center place-items-center 
-        grid-flow-col gap-x-4  col-span-full  place-content-evenly  border-b
-        bg-stone-300/85  dark:bg-background/80  sm:border-1  border-muted '
->
-
-<Breadcrumb.Root  class ='inline-grid  invisible   md:visible  rounded-lg  bg-muted/80 dark:bg-muted
-                          col-start-2   col-end-7  px-4  py-2   contrast-200 justify-self-start '
->
-  <Breadcrumb.List  class= 'text-[2ch]' >
-    <Breadcrumb.Item> 
-      <Breadcrumb.Link href='##'> Dashboard </Breadcrumb.Link>
-    </Breadcrumb.Item>
-    <Breadcrumb.Separator />
-    <Breadcrumb.Item>
-      <Breadcrumb.Link  href='##'> Orders </Breadcrumb.Link>
-    </Breadcrumb.Item>
-    <Breadcrumb.Separator />
-    <Breadcrumb.Item>
-      <Breadcrumb.Page> Recent Orders </Breadcrumb.Page>
-    </Breadcrumb.Item>
-  </Breadcrumb.List>
-</Breadcrumb.Root>
-
-  <span  class ='inline-grid  col-start-10 col-end-13 gap-x-0.5 justify-evenly '>
-
-    <Dark_Light />
-
-    <DropdownMenu.Root >
-    <DropdownMenu.Trigger asChild let:builder>
-        <Button
-                size='icon'
-                class='size-12  col-start-11 overflow-hidden rounded-full transition-all duration-300
-                      hover:scale-150  border-black border-2 select-none  shadow-xl shadow-blue-800 self-center'
-                builders={[builder]}
-        >                   <!-- /images/placeholder-user.jpg' '~icons/lucide/users' -->
-            <img    alt   ='Avatar'
-                    src   ='https://avatars.githubusercontent.com/u/26680960?v=4'
-                    class ='overflow-hidden self-stretch rounded-full'
-            />
-        </Button>
-    </DropdownMenu.Trigger>
-
-    <DropdownMenu.Content  align='end'>
-        <DropdownMenu.Label> Account: </DropdownMenu.Label>  <DropdownMenu.Separator/>
-        <DropdownMenu.Item>  Settings </DropdownMenu.Item>
-        <DropdownMenu.Item>  Support </DropdownMenu.Item>   <DropdownMenu.Separator/>
-        <DropdownMenu.Item>  Logout  </DropdownMenu.Item>
-    </DropdownMenu.Content>
-    </DropdownMenu.Root>
+  
+  
+  <!--class = "border-muted {Right? 'pr-10 ' :'pl-10 '} "-->
+<span   class = "{fixed? 'fixed' :'portrait:fixed absolute'} w-full h-fit  m-0 p-0 {!S_bar? 'px-[1.2ch]' :Right? 'pr-[3.2ch]'  :'pl-[3.2ch]'} 
+            inline-grid  grid-cols-12  top-0 z-50  min-h-[7.4ch] portrait:min-h-[8ch]  border-b-2  sm:border-b  border-black
+            items-center justify-stretch  place-items-center  place-content-evenly  dark:border-gray-400/70
+            grid-flow-col  col-span-full  transition-all  duration-1000  bg-gray-300/60  dark:bg-[#161819] 
+            gap-x-2  shadow-lg   hover:shadow-muted-foreground  shadow-stone-500/80 "
+  >
+  
+  <Breadcrumb.Root  class ='inline-grid  invisible  md:visible   bg-muted   rounded-lg  contrast-200
+                            col-start-2  col-end-8  portrait:col-start-3   px-2  py-2  justify-self-start '
+  >
+    <Breadcrumb.List  class= 'text-md  landscape:text-lg ' >
+      <Breadcrumb.Item class='portrait:hidden '>Dashboard</Breadcrumb.Item>
+      <Breadcrumb.Separator class={!On_Page? 'hidden '  :'portrait:hidden '}/>
+      <Breadcrumb.Item >   <!-- {activeElement.nodeName=='A' && items.includes(Active_Itm)? Active_Itm :'Dashboard'} -->
+        <Breadcrumb.Link  href='#{On_Page}'  class={Active_Itm[0]!=='#'? 'hidden '  :' '}> {On_Page} </Breadcrumb.Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Separator   class={On_Page!=='Orders' || !itm? 'hidden ' :'portrait:hidden '}/>
+      <Breadcrumb.Item    class={On_Page==='Orders'? '' :'portrait:hidden '}>
+        <Breadcrumb.Page  href='#{itm}'> {On_Page==='Orders'? 'Recent Orders'  :On_Page} </Breadcrumb.Page>
+      </Breadcrumb.Item>
+  
+    </Breadcrumb.List>
+  </Breadcrumb.Root>
+  
+    <span  class ='inline-grid  {Right? 'col-start-8 col-end-11' :'col-start-9' } col-end-13 portrait:col-end-11 gap-2  mt-2  justify-evenly *:transition-all *:duration-300 '>
+      <Search_Bar  {On_Page} {Active_Itm} />             
+      <Fullscreen />
+      <!--    _________________KB shortcuts___________________
+                  [alt + /]   (toggle Search_Bar on/off)
+                  [Escape]    (clear input text 1st time or toggle
+                              Search_Bar if input text is clear) 
+      -->
+      <Dark_Light />
+  
+      <DropdownMenu.Root >
+      <DropdownMenu.Trigger asChild let:builder>
+          <Button
+                  size='icon'
+                  class='size-12 grid-cols-subgrid col-start-11 portrait:col-end-12 overflow-hidden rounded-full transition-all duration-200
+                        hover:scale-150  border-black border-2 select-none  shadow-xl shadow-blue-800 self-center'
+                  builders={[builder]}
+          >                   <!-- /images/placeholder-user.jpg' '~icons/lucide/users' -->
+              <img    alt   ='Avatar'
+                      src   ='https://avatars.githubusercontent.com/u/26680960?v=4'
+                      class ='overflow-hidden self-stretch rounded-full'
+              />
+          </Button>
+      </DropdownMenu.Trigger>
+  
+      <DropdownMenu.Content  align='end'>
+          <DropdownMenu.Label> Account: </DropdownMenu.Label>  <DropdownMenu.Separator/>
+          <DropdownMenu.Item>  Settings </DropdownMenu.Item>
+          <DropdownMenu.Item>  Support </DropdownMenu.Item>   <DropdownMenu.Separator/>
+          <DropdownMenu.Item>  Logout  </DropdownMenu.Item>
+      </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </span>
+  
   </span>
-
-</span>
