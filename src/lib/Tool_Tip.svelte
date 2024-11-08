fda2e73e30a0056ed6bb1f7152f0bd865b4c3f0d
peyman-borhani <peyman.borhani@gmail.com>
2024-11-08 08:25:34 +0330

    +Add-New:  component => Tool_Tip

---
+++ src/lib/Tool_Tip.svelte
@@ -0,0 +1,25 @@
+<script>
+import  {Root, Trigger, Content}    from  '$lib/components/ui/tooltip/index.js';
+
+let  {t, css, children} =$props()
+let hov = false
+
+</script>
+
+
+<Root>
+    <Trigger  asChild  let:builder >
+      <span
+            class  ='inline-grid  items-center  justify-center   place-content-center 
+                     text-foreground/60  hover:text-foreground   rounded-md  text-black
+                     hover:scale-150  transition-all  duration-200   dark:text-foreground '
+            onpointerover   ={()=>hov=true}
+            onpointerleave  ={()=>hov=false}
+            use:builder.action   {...builder}
+        >
+            {@render children()}
+            <span  class='sr-only'>{t}</span>
+      </span>
+    </Trigger>
+    <Content >{t}</Content>
+</Root>
