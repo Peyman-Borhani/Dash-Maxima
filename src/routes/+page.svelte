b02e97eae34303f52420547fb60fa72f157a629f
peyman-borhani <peyman.borhani@gmail.com>
2024-11-13 18:07:35 +0330

    +Improve: sizing => witdh, fonts, portrait, scaling +Improve: styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,14 @@
  
 </script>
 
-<!-- User's content or in another word, "App's main Content" is placed in main element of the base page route  -->
-<main   class=" grid  grid-cols-12  z-10  h-fit {!P_sbar?'w-[99svw]' :!S_bar?'w-[94svw]' :P_sbar?'w-[96svw] lg:w-[85svw]' :'w-[86svw]'} landscape:w-[86svw] border border-black/70 dark:border-black/60
-                self-start place-self-center  col-span-full row-start-3  row-end-auto auto-rows-auto  bg-[#ccd8] dark:bg-background/80  rounded-lg  px-3 landscape:px-5  portrait:py-6 
-                {!P_sbar && !S_bar? 'mx-[4svw]' :Right? 'portrait:absolute portrait:lg:left-1.5 landscape:-ml-[8.6svw] ' :'portrait:absolute portrait:lg:right-1.5 landscape:ml-[9.2svw] '}"
-        style ='box-shadow: 0 0 6ch #112a, inset 0 0 1ch #102, inset 0 0 16ch #879a;'
+<!-- User's content or in another word, "App's main Content" is placed in main element of the base page route   -->
+<main   class=" grid  grid-cols-12  z-10  h-fit {!P_sbar?'w-[100svw]' :!S_bar?'w-[94svw]' :P_sbar?'w-[100svw] lg:w-[88svw]' :'w-[86svw]'} landscape:w-[86svw] border border-black/70 dark:border-black/60
+                self-start place-self-center  col-span-full row-start-3  row-end-auto auto-rows-auto  rounded-lg  p-2 portrait:p-3  landscape:px-5  portrait:pb-20 bg-[#bbc8] dark:bg-gray-900/80 
+                {!P_sbar && !S_bar? 'mx-[1svw] landscape:mx-[3svw]' :Right? 'portrait:absolute portrait:left-0 landscape:-ml-[9svw] ' :'portrait:absolute portrait:right-0 landscape:ml-[9svw] '}
+                bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))]  from-[#bbc] via-[#ccd] to-[#def]  dark:from-[#334] dark:via-[#445] dark:to-[#121113] 
+
+            "
+        style ='box-shadow: 2pt 6pt  2ch #223, 0 0  3ch #aab, inset 0 0 3pt #000, inset 0 0 2ch #201830, inset 0 0 12svmax #768b;'
 >       
-    <Content  Tdata={data.table} /> 
+    <Content  Tdata={data.table} />
 </main>
