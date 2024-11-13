b2308646848a3ae5a1778cec789ce079ec6b43a1
peyman-borhani <peyman.borhani@gmail.com>
2024-11-13 18:24:19 +0330

    +Add: icon => Github +Improve: styles => spacing, space usage, placements, logics +Fix: link route => Home +Update-Fix: UI => sidebar logic, changes, side... impact on UI

--- src/lib/Side_Bar.svelte
+++ src/lib/Side_Bar.svelte
@@ -7,15 +7,17 @@
     import  Settings      from  '~icons/lucide/settings'
     import  ShoppingCart  from  '~icons/lucide/shopping-cart'
     import  Tags          from  '~icons/lucide/tags'
-    import  Cross         from  '~icons/radix-icons/cross2'
+    import  GitHub        from  '~icons/radix-icons/github-logo'
+    import  Cross         from  '~icons/radix-icons/cross2';
+
     import  * as Sheet      from  '$lib/components/ui/sheet/index.js'
     import  {Button}        from  '$lib/components/ui/button/index.js'
     import  * as Tooltip    from  '$lib/components/ui/tooltip/index.js'
-    import  {activeElement} from  'runed'
+    import  {activeElement} from  'runed';
 
 
 let     {cfg,    Active_Itm,    On_Page=$bindable('Home'),
-        items= ['Home', 'Orders', 'Products', 'Customers', 'Analytics', '', 'Settings']
+        items= ['Home', 'Orders', 'Products', 'Customers', 'Analytics', '', '', 'Settings']
 }=$props();     //max word size among items. min: 2 (char+icon) //const menu_size = `${items.reduce((x,itm)=> {return itm.length>x? itm.length :x}, 2)}ch`;
 
 //let  fixed=cfg.fixed,  P_sbar=cfg.P_sbar,  S_bar=cfg.S_bar,  Right=cfg.Right,  menu=cfg.menu;
@@ -30,9 +32,9 @@ let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
             no_menu [force no menu on both (portrait too)]
 */
  const  menu_css =`flex  w-fit items-center justify-items-between gap-4 p-3  hover:scale-125  transition-transform  whitespace-nowrap *:size-9 
-                      ${Right? 'flex-row-reverse self-start' : 'justify-self-end '} text-muted-foreground hover:text-foreground hover:text-foreground self-stretch `
+                    ${Right? 'flex-row-reverse self-start' : 'justify-self-end '} text-muted-foreground hover:text-foreground hover:text-foreground self-stretch `
         ,
-        icon_css ="size-8  group-hover:scale-150  transition-all duration-200 rounded-md " ;
+        icon_css ='size-8  group-hover:scale-150  transition-all duration-200  rounded-md ';
 
  const  side  = Right? 'left'    :'right'; //Tooltip popup side
     //sidebar portrait visiblity
@@ -41,11 +43,11 @@ let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
                     :(S_bar && !P_sbar)? 'portrait:hidden portrait:h-0 landscape:inline-grid  landscape:h-full landscape:row-start-2 landscape:row-end-13 '
                     :' ';
                  
- const  sbar_css = ` py-2.5   dark:shadow-stone-50  transition-all duration-300
-            ${fixed?  'fixed '  :'absolute '} 
-            ${P_sbar? 'lg:h-screen  lg:grid-rows-12 ' : ' '}
-            ${!S_bar? 'h-fit grid-rows-1 row-span-1 p-2.5 '  :'landscape:h-screen  landscape:grid-rows-12 '} 
-            ${Right?  'col-start-13 -col-end-1  border-l  justify-self-end '  :'col-start-0 col-end-1  border-r '}
+ const  sbar_css = ` 
+            ${fixed?  'fixed'  :'absolute'}  rounded-ee-xl  
+            ${S_bar&&P_sbar? 'lg:h-screen  lg:grid-rows-12  lg:portrait:rounded-none ' :S_bar? 'landscape:rounded-none ' :' '} 
+            ${!S_bar? 'h-fit grid-rows-1 row-span-1 p-2.5 '  :'landscape:h-screen  landscape:grid-rows-12 '}  
+            ${Right?  'col-start-13 -col-end-1  border-l  justify-self-end '  :'col-start-0 col-end-1  border-r '} 
             `;
 </script>
 
@@ -62,7 +64,8 @@ let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
 
 
 
-<aside   class ="inline-grid z-50  grid-flow-row  bg-background  {sbar_css}  content-between  select-none 
+<aside   class ="inline-grid z-50  grid-flow-row  bg-[#bbc] dark:bg-background  content-between
+                {sbar_css} py-2.5   dark:shadow-stone-50  transition-all duration-300  select-none 
                 {S_bar? 'landscape:shadow-[0_9svh_4svw_#889a,0_9svh_1svw_#000]  landscape:dark:shadow-[0_10svh_4svw_#ccda,0_10svh_0.4svw_#eefa] ' : ' '} 
                 "
 >
@@ -71,10 +74,10 @@ let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
     <Sheet.Trigger  asChild  let:builder 
     >
         <Button     builders ={[builder]}   size='icon'     variant='outline'
-                    class ="row-start-1 row-end-2 mx-1.5 place-self-center  justify-self-center size-fit z-50 {Right? 'mr-2' :'ml-2'}
-                            max-h-[5.8ch] max-w-[7ch] bg-inherit  shadow-[0_0_1ch_#666]  rounded-lg  hover:scale-125 transition-transform "
+                    class ="row-start-1 row-end-2 mx-1.5 place-self-center  justify-self-center size-fit z-50 {Right? 'mr-2' :'ml-2'} 
+                            max-h-[6.2ch]  max-w-[7ch] bg-inherit  shadow-[0_0_1ch_#666]  rounded-lg  hover:scale-125 transition-transform "
         >                   
-            <Menu   class ='size-12 '/>
+            <Menu   class ='size-12'/>
             <span   class ='sr-only'> Toggle Menu </span>
         </Button>
     </Sheet.Trigger>
@@ -106,24 +109,24 @@ let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
 <!------------------------- Side_Bar ---------------------------->
 
   {#if S_bar || (S_bar && P_sbar)}
-    <nav  class="{sbar_vis}  grid-flow-row  pt-4  justify-evenly bg-inherit  px-2.5 landscape:px-5 group-hover:scale-150 
-                place-content-evenly  {Right? 'justify-start pl-3.5' :'justify-end  pr-3.5'}  "
+    <nav  class="{sbar_vis}  grid-flow-row  pt-9  justify-evenly bg-inherit  px-2 landscape:px-5 group-hover:scale-150 
+                place-content-between  {Right? 'justify-start pr-4' :'justify-end  pl-4'}  "
     > 
-      {#each items  as  itm }
-      {#if itm}
+      {#each Array(items.length)  as  itm, i }
+      {#if items[i]}
         <Tooltip.Root>
             <Tooltip.Trigger  asChild  let:builder >
-            <a  href='#{itm}'
+            <a  href="{items[i]==='Home'? '/'  :('#'+items[i]) }"
                 class='inline-grid  group place-content-center  last:row-start-12 
                        rounded-md text-foreground/60 transition-all  hover:text-foreground  *:size-8 '
-                onpointerup={()=>On_Page=itm}
+                onpointerup={()=>On_Page=items[i]}
                 use:builder.action   {...builder}
             >
-                {@render Icon(itm,`hover: ${icon_css}  ${On_Page===itm? 'dark:text-foreground text-black shadow-[1px_1pt_1svh_#ddea]  dark:shadow-none  drop-shadow-[1px_1px_.1svmax_#000] dark:drop-shadow-[0_2pt_0.6svw_#fff] rounded-lg size-10 scale-150' :'shadow-lg shadow-muted'} `) }
-                <span  class='sr-only'>{itm}</span>
+                {@render Icon(items[i],`${icon_css}  ${On_Page===items[i]? 'dark:text-foreground text-black shadow-[1px_1pt_1svh_#ddea]  dark:shadow-none  drop-shadow-[1px_1px_.1svmax_#000] dark:drop-shadow-[0_2pt_0.6svw_#fff] rounded-lg size-10 scale-150' :'shadow-lg shadow-muted'} `) }
+                <span  class='sr-only'>{items[i]}</span>
             </a>
             </Tooltip.Trigger>
-            <Tooltip.Content {side}>{itm}</Tooltip.Content>
+            <Tooltip.Content {side}>{items[i]}</Tooltip.Content>
         </Tooltip.Root>
       {/if}
       {/each}
@@ -133,7 +136,7 @@ let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
       <Tooltip.Root>
         <Tooltip.Trigger asChild let:builder>
             <a  href='##'
-                class='{icon_css} row-start-6  row-end-8  text-xl '
+                class='{icon_css} row-start-6  row-end-7  text-xl '
                 use:builder.action    {...builder}
             >
                 <Tags   class='hover:scale-150  transition-all duration-300 font-sans size-8 justify-center' 
@@ -145,8 +148,28 @@ let  {fixed,  P_sbar,  S_bar,  Right,  menu, no_menu} = cfg;
         <Tooltip.Content {side}  class ='ml-2 p-4 italic text-md font-semibold text-violet-600'>
             Tag-name: 
             <span class='text-lg text-green-500'> {activeElement.current?.nodeName} </span>
-            &nbsp - &nbsp Item: 
+                &nbsp - &nbsp Item: 
             <span class='text-lg  text-green-500'> {activeElement.current?.textContent} </span>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    {/if}
+
+    {#if  !items[items.length-3]}
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+            <a  href='https://github.com/Peyman-Borhani/Dash-Maxima'
+                class='{icon_css} row-start-7  row-end-8 mb-6 text-xl '
+                use:builder.action    {...builder}
+            >
+            <GitHub   class= ' hover:scale-150  transition-all duration-300 font-sans size-8 text-black bg-background dark:bg-muted-foreground shadow-[inset_0_0_2pt_#000_,_inset_0_0_1.6ch_#102] rounded-full opacity-80 hover:opacity-100 hover:brightness-150' 
+                       style='text-shadow: 0 0 1ch #def; '
+                />  
+                <span  class='sr-only'>GitHub</span>
+            </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content {side}  class ='ml-2 p-4 italic text-md font-semibold text-violet-600'>
+            will open repo:  
+            <span class='text-lg text-blue-600 '> &nbsp https://github.com/Peyman-Borhani/Dash-Maxima  </span>
         </Tooltip.Content>
       </Tooltip.Root>
     {/if}
