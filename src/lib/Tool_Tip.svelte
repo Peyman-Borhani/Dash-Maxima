b334d0b0ba431ad62f737808ee85c17678099c06
peyman-borhani <peyman.borhani@gmail.com>
2024-11-13 18:09:27 +0330

    -Remove => unused function

--- src/lib/Tool_Tip.svelte
+++ src/lib/Tool_Tip.svelte
@@ -13,8 +13,6 @@ let hov = false
             class  ='inline-grid   w-fit   {css} mx-auto  items-center   justify-between   
                      hover:text-foreground   rounded-md  text-black   place-content-between
                      hover:scale-150  transition-all  duration-200   dark:text-foreground '
-            onpointerover   ={()=>hov=true}
-            onpointerleave  ={()=>hov=false}
             use:builder.action   {...builder}
         >
             {@render children()}
