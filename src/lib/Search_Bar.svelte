2d4844c427770c8bec533ad03e1127e8c92127cc
peyman-borhani <peyman.borhani@gmail.com>
2024-11-13 18:28:55 +0330

    +Improve: styles UI => sizing,  placement, logic +Fix: logic => UI impact +Fix: styles => portrait, left/right

--- src/lib/Search_Bar.svelte
+++ src/lib/Search_Bar.svelte
@@ -2,8 +2,9 @@
     import  Search      from  '~icons/lucide/search';
     //import  {Input}     from  '$lib/components/ui/input/index.js';
     
-    let  {On_Page,  input,  Active_Itm}  =$props();
+    let  {On_Page, Right, input, Active_Itm}  =$props();
     let  {view, value, inp}  =$state('');
+    
     let  srch;
 
 function  keyIn(e) 
@@ -19,20 +20,22 @@ function  keyIn(e)
 }
 </script>
 
-<!--svelte:options accessors / -->
+<!--svelte:options accessors / portrait:-translate-x-16 -->
 
 <svelte:window   onkeyup ={keyIn} />
 
-<div  class ="inline-grid  absolute  items-center   portrait:fixed    portrait:bottom-8  select-none bg-none  opacity-100
-            {view? 'fixed  rounded-r-xl  scale-150  landscape:-translate-x-52 portrait:-translate-x-16 mt-28'  :'rounded-full  hover:scale-150 portrait:opacity-80'}
-            portrait:right-4  z-50  self-center  place-items-center justify-self-start  rounded-full bg-none scale-110  transition-all duration-200 "
-        style ='grid-template-columns: 90% 10%; '
+<div  class ="inline-grid  absolute  w-fit  items-center   portrait:fixed  bg-none  opacity-100
+            {Right? 'portrait:left-4' :'portrait:right-3'}   portrait:bottom-8  select-none 
+            {view? 'fixed  rounded-r-xl  scale-150  landscape:-translate-x-60  landscape:mt-28'  :'rounded-full  hover:scale-150  portrait:opacity-80'} 
+            {view&&Right? 'portrait:left-20  ' :view? 'portrait:right-24 ' :''}
+            z-50  self-center  place-items-center justify-self-start  rounded-full bg-none scale-110  transition-all duration-200 "
+        style =' grid-template-columns: 90% 10%; '
         
 >
 {#if view}
     <input  type  ='text'       bind:value={value}      bind:this={inp}
             style ='box-shadow:  0 0 4pt #203, 0 0 .7ch #cde, inset 0 0 9pt #bad8;  padding: 0.4ch .6ch;'
-            class ='inline-grid  w-full h-full z-50  text-center  place-items-end  outline-none border-muted  transition-all duration-200   
+            class ='inline-grid  w-[50svw] h-full z-50  text-center  place-items-end  outline-none border-muted  transition-all duration-200   
                     py-4  landscape:w-[36ch]  rounded-l-xl  placeholder-opacity-20 focus:opacity-100 dark:bg-black '
             placeholder={On_Page?  On_Page  :Active_Itm? Active_Itm  :'Search...'} contenteditable
             onchange ={keyIn}
