fae9ca82f30b1475b2c77013d48db97fcb8e797f
peyman-borhani <peyman.borhani@gmail.com>
2024-11-20 00:58:57 +0330

    +Add: Srch_Top => Btn Top Bottom position ~Refactor +Improve: (logic, styles) => fine tuning +Add-fix: extras => custom interactive behaviour.

--- src/lib/Search_Bar.svelte
+++ src/lib/Search_Bar.svelte
@@ -2,54 +2,61 @@
     import  Search      from  '~icons/lucide/search';
     //import  {Input}     from  '$lib/components/ui/input/index.js';
     
-    let  {On_Page, Right, input, Active_Itm}  =$props();
-    let  {view, value, inp}  =$state('');
-    
-    let  srch;
+    let  {Srch_top, P_sbar, Right, input, Active_Itm}  =$props();
+
+    let  {view, value, inp, Sbind}  =$state('');
+    let  Srch = $state(false);
+
+    const tap  =()=> {
+                        if(view && Active_Itm==='Page Body') view=false;
+                        else if(Active_Itm==='Search') {Srch=true;  inp.focus()} 
+                        else if(Srch)  Srch=false;
+    }
+
 
 function  keyIn(e) 
-{//console.log(e);
-    if( (e.key==='Escape' && value==='') || (e.key==='/' && e.altKey) )
-        {view =!view;   value=''}
+{
+    if((e.key==='Escape' && value==='') || (e.key==='/' && e.altKey) )
+        {view =!view;   if(view){Srch=true;  inp.focus(); }  else value='';  }
     else if (e.key==='Escape') {value='';   input=''}
     else {  
         inp.focus();
         if( e.key==='Enter' || e.type==='change') 
-          {input=value;  value='';   inp.blur(); srch.blur(); view=false}
+          {input=value;  value='';   inp.blur(); Sbind.blur(); view=false; Srch=false}
     }
 }
 </script>
 
 <!--svelte:options accessors / portrait:-translate-x-16 -->
 
-<svelte:window   onkeyup ={keyIn} />
+<svelte:window  onpointerup={tap}  onkeyup={keyIn} />
 
-<div  class ="inline-grid  absolute  w-fit  items-center   portrait:fixed  bg-none  opacity-100
-            {Right? 'portrait:left-4' :'portrait:right-3'}   portrait:bottom-8  select-none 
-            {view? 'fixed  rounded-r-xl  scale-150  landscape:-translate-x-60  landscape:mt-28'  :'rounded-full  hover:scale-150  portrait:opacity-80'} 
-            {view&&Right? 'portrait:left-24  ' :view? 'portrait:right-28 ' :''}
+<div    id ='Search' 
+        class ="inline-grid  portrait:fixed  px-2 items-center  bg-none  portrait:opacity-80   select-none
+            {!view&&Right? 'portrait:left-2' :!view? 'portrait:right-7' :''}   {Srch_top? 'portrait:top-20' :'portrait:bottom-8'} 
+            {view? 'fixed h-fit portrait:left-4  portrait:-ml-10 lg:portrait:mx-0 portrait:w-[96%] rounded-r-xl landscape:-translate-x-[29svw] landscape:mt-28'  :'rounded-full  hover:scale-150  portrait:opacity-100'} 
+            {P_sbar&&view&&Right? 'lg:portrait:-ml-[16%] ' :P_sbar&&view? 'lg:portrait:-ml-[4%] '  :''}
             z-50  self-center  place-items-center justify-self-start  rounded-full bg-none scale-110  transition-all duration-200 "
-        style =' grid-template-columns: 90% 10%; '
-        
+        style =' grid-template-columns: 90% 10%; '    
 >
-{#if view}
+
     <input  type  ='text'       bind:value={value}      bind:this={inp}
-            style ='box-shadow:  0 0 4pt #203, 0 0 .7ch #cde, inset 0 0 9pt #bad8;  padding: 0.4ch .6ch;'
-            class ='inline-grid  w-[50svw] h-full z-50  text-center  place-items-end  outline-none border-muted  transition-all duration-200   
-                    py-4  landscape:w-[36ch]  rounded-l-xl  placeholder-opacity-20 focus:opacity-100 dark:bg-black '
-            placeholder={On_Page?  On_Page  :Active_Itm? Active_Itm  :'Search...'} contenteditable
+            style ='box-shadow:  0 0 4pt #203, 0 0 .7ch #cde, inset 0 0 9pt #bad8;  font-size: 2.6ch;   padding: .2ch 1ch; '
+            class ='inline-grid  z-50  h-full  text[2.6ch]  text-center  portrait:-mr-[24%]  place-items-end transition-all duration-500
+                    {view? 'visible opacity-100 landscape:w-[36ch]  portrait:w-[90%] lg:portrait:w-[80%]'  :'invisible opacity-0 landscape:w-0  portrait:w-0 lg:portrait:w-0'}
+                    rounded-l-xl  placeholder-opacity-20 focus:opacity-100 dark:bg-black  outline-none border-muted '
+            placeholder={Srch? 'Search...' :Active_Itm? Active_Itm :'Search...'} contenteditable
             onchange ={keyIn}
     >
-{/if}
-    <button   onpointerup={()=> view=!view}  bind:this={srch}
-              class ='size-10  z-50  {view? 'size-9 rounded-r-xl scale-100 border border-[#a9c]' :'rounded-full portrait:scale-125 '} justify-end  *:hover:text-2xl
-                      bg-muted  dark:bg-black   transition-all  duration-200 outline-none  hover:contrast-125 dark:hover:brightness-150  *:transition-all '
+    <button   id='SrBtn'  bind:this={Sbind}  onpointerup= {_=>view=!view}
+              class ='size-11  z-50  {view? 'rounded-r-xl  border border-[#a9c]   ' :'rounded-full '} justify-end *:hover:text-2xl 
+                      bg-muted  dark:bg-black  transition-all  duration-600 outline-none  hover:contrast-125 dark:hover:brightness-150 '
               style ='box-shadow: 0 0 4pt #203, 0 0 1.2ch #def, inset 0 0 3.6pt #cde, inset 0 0 1ch #51b; '  
     >   
-        {#if view}  <span   class ='text-muted-foreground  font-light  text-lg'> X </span>
-        {:else}     <Search  class='text-gray-800 dark:text-gray-500 translate-x-1.5 text-xl' />
+        {#if view}  <span     class='text-[#96b]  text-lg transition-all duration-200 hover:text-red-700'> X </span>
+        {:else}     <Search   class='text-gray-800 dark:text-gray-500 translate-x-2 text-2xl '/>
         {/if}
-        <span class='sr-only'> Search/close button </span>
+        <span class='sr-only'>Search</span>
     </button>
 
 </div>
