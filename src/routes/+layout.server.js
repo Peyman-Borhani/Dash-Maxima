83c424751a7b559332976a0fded4ecd899d3e722
peyman-borhani <peyman.borhani@gmail.com>
2025-02-06 12:10:05 +0330

    +Add-cfg: customizeble states:  H_sbar , H_hbar =>  bars state: side & head Menu , No_Menu: Portrait => aspect ratio Scroll => user  scroll state, action, behaviour Visited => cookie saves user settings

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -4,12 +4,15 @@ export  const  prerender  =true;
 import  {DB}    from  './data.server.js';
 
 
-export  function  load({cookies}){ 
-
+export  function  load({cookies})
+{ 
   const   visited = cookies.get('visited');
 
   return {
     DB
+    ,
+    //____Sidebar items________________
+    NAV_ITEMS: ['Home', 'Orders', 'Products', 'Customers', 'Analytics', '', '', 'Settings']
     ,
     CONFIG:     //___UI settings, set's Dash state in all routes________ 
     {   
@@ -19,16 +22,19 @@ export  function  load({cookies}){
         F_bar :  false,     // T-> Footer   F->  none/hidden
         L_sbar:  true,      // T-> L_sbar    F->  no Side_Bar at all
         P_sbar:  true,      // T-> force Side_Bar in portrait
+        H_sbar:  true,      // T-> Drop down side bar(drawer hide/unhide)  F-> fixed
+        H_hbar:  true,      // T-> Drop down header bar(drawer hide/unhide)  F-> fixed
         No_bar:  false,     // T-> No Dashboard (no Side_Bar, no Header... just an exit full Content button)
-        Hide  :  false,     // T-> hides Dashboard by the edge (Bars trigger by scroll-down & pointer near the edge)
-        menu  :  true,      // T-> Menu panel   F->  hidden/no menu
-        no_menu : false,    // T-> no Menu panelin any condition
+        Hide  :  true,      // T-> hides Dashboard by the edge (Bars trigger by scroll-down & pointer near the edge)
+        Menu  :  true,      // T-> Menu panel   F->  hidden/no menu
+        No_Menu : false,    // T-> no Menu panel in any condition
         SrBtn_Up: true,    // T-> Searchbar Always on Top (also Portrait)
         On_Page : 'Home',   // current landing item/page focus
         User    : {name:'Peyman', logged: false, input: ''},
-        visited : visited===true
+        Portrait: false,
+        Visited : visited===true,
+        Scroll  : {at:'start', action:'none', path:'none'}
+                //start page end    //down up hold none
     }
-    ,//____Sidebar items________________
-    NAV_ITEMS: ['Home', 'Orders', 'Products', 'Customers', 'Analytics', '', '', 'Settings']
   }
 }
